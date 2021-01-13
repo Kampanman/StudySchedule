33584fb979c2797d18720dc0fd6de4b92691b710
Kampanman <55080133+Kampanman@users.noreply.github.com>
2021-01-13 23:34:07 +0900

    Add files via upload

--- myStudySchedule.php
+++ myStudySchedule.php
@@ -87,21 +87,7 @@
                         </button>
                         </div>
                         <div class="modal-body">
-                        <p>
-                            <b>【共通】</b><br>
-                            ・1st: 学習初日　2nd: 学習初日の翌日　3rd: 2ndの3日後（学習初日の4日後）<br><br>
-                            <b>【Type_1】</b><br>
-                            ・4th: 3rdの4日後（学習初日の8日後） 5th: 4thの7日後（学習初日の15日後）<br><br>
-                            <b>【Type_2】</b><br>
-                            ・4th: 3rdの5日後（学習初日の9日後） 5th: 4thの7日後（学習初日の16日後）<br><br>
-                            <b>【Type_3】</b><br>
-                            ・4th: 3rdの4日後（学習初日の8日後） 5th: 4thの7日後（学習初日の15日後）<br><br>
-                            <b>【Type_4】</b><br>
-                            ・4th: 3rdの5日後（学習初日の9日後） 5th: 4thの7日後（学習初日の16日後）<br>
-                        </p>
-                        <br>
-                        <p><b>　Type_1・2のタスクの学習開始日と、Type_3・4のタスクの学習開始日は、それぞれ同日にするとよいでしょう。<br>
-                        　また、Type_2とType_3の学習日は中1日、Type_4Type_1の学習日は中3日空けるとよいでしょう。</b></p>
+                        
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -118,22 +104,23 @@
 </html>
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="modalInner.js"></script>
 <script type="text/javascript">
 $('#example').DataTable({
-        responsive: true,
-        lengthChange: true,
-        info: false,
-        searching: true,
-        paging: true,
-        pagingType: "full_numbers",
-        lengthMenu: [ 10, 20, 50, 100 ],
-        columnDefs: [
-          { targets: 0, visible: false }
-        ],
-        language: {
-            url: "//cdn.datatables.net/plug-ins/1.10.20/i18n/Japanese.json"
-        }
-    });
+    responsive: true,
+    lengthChange: true,
+    info: false,
+    searching: true,
+    paging: true,
+    pagingType: "full_numbers",
+    lengthMenu: [ 10, 20, 50, 100 ],
+    columnDefs: [
+        { targets: 0, visible: false }
+    ],
+    language: {
+        url: "//cdn.datatables.net/plug-ins/1.10.20/i18n/Japanese.json"
+    }
+});
 </script>
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
 </body>
